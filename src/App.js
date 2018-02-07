36a6166584399f8d58f89f7f5af6c4404a90d2f3
Max Grechko <gremax@gremax.me>
2018-02-07 22:25:04 +0200

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <div className="gray-bg">
+        <Grid>
+          <Row>
+            <Col lg={9}>
+              <div className="wrapper wrapper-content">
+                <h2>Dashboard</h2>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     )
   }
 }
