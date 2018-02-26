a19d668248463b085aa57c17b61880f79670c373
Max Grechko <gremax@gremax.me>
2018-02-26 18:51:47 +0200

    Add styles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css'
+import 'jquery/src/jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'font-awesome/css/font-awesome.min.css'
+import './assets/styles/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
