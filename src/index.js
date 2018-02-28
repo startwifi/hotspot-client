5b60010f8f4c833efbcb0cd6a7e7c8d39bc80ceb
Max Grechko <gremax@gremax.me>
2018-02-28 21:55:09 +0200

    Configure redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './App'
+import reducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker'
 
 import 'jquery/src/jquery'
@@ -10,10 +13,14 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'font-awesome/css/font-awesome.min.css'
 import './assets/styles/style.css'
 
+const store = createStore(reducer)
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+import * as actionTypes from './actions'
+
+const initialState = {
+}
+
+const reducer = (state = initialState, action) => {
+}
+
+export default reducer
