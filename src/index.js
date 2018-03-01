5b9e082113b06a52892d5af2ef0612669d758100
Max Grechko <gremax@gremax.me>
2018-03-01 20:51:37 +0200

    Add redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
 import App from './App'
 import reducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker'
@@ -13,7 +14,15 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'font-awesome/css/font-awesome.min.css'
 import './assets/styles/style.css'
 
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const rootReducer = combineReducers({
+  reducer: reducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+))
 
 const app = (
   <Provider store={store}>
