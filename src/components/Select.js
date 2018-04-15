53f244e6f6e31d42b02d2d6e5d672230ed74f894
Max Grechko <gremax@gremax.me>
2018-04-15 13:40:13 +0300

    Add Select component

---
+++ src/components/Select.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import classnames from 'classnames'
+
+const select = props => {
+  const {
+    input,
+    label,
+    type,
+    options,
+    meta: { touched, error, invalid, warning }
+  } = props
+
+  const divClasses = classnames({
+    'form-group': true,
+    'has-error': !!(touched & invalid)
+  })
+
+  return (
+    <div className={divClasses}>
+      <label className="col-md-2 control-label">{label}</label>
+      <div className="col-md-10">
+        <select {...input} type={type} className="form-control">
+          {options}
+        </select>
+        <div className="help-block">
+          {touched &&
+            ((error && <span>{error}</span>) ||
+              (warning && <span>{warning}</span>))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default select
