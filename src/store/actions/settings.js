e77730bd2e7d4e3494593e67075248bd22cd47bf
Max Grechko <gremax@gremax.me>
2018-05-28 21:21:38 +0300

    Add settings actions

---
+++ src/store/actions/settings.js
@@ -0,0 +1,91 @@
+import * as actionTypes from './actionTypes'
+import axios from 'utils/api'
+import normalize from 'json-api-normalizer'
+
+export const updateCompanySettingsStart = () => {
+  return {
+    type: actionTypes.UPDATE_COMPANY_SETTINGS_START
+  }
+}
+
+export const updateCompanySettings = (token, id, attributes) => {
+  const requestData = {
+    data: {
+      type: 'settings',
+      id: id,
+      attributes: {
+        name: attributes.name
+      }
+    }
+  }
+  return dispatch => {
+    dispatch(updateCompanySettingsStart())
+    axios.defaults.headers.common['Authorization'] = `bearer ${token}`
+    axios
+      .patch(`/settings/${id}`, requestData)
+      .then(res => {
+        dispatch(updateCompanySettingsSuccess(res.data.data))
+      })
+      .catch(error => {
+        dispatch(updateCompanySettingsFailure(error.response.data.errors))
+      })
+  }
+}
+
+export const updateCompanySettingsSuccess = settings => {
+  return {
+    type: actionTypes.UPDATE_COMPANY_SETTINGS_SUCCESS,
+    payload: settings
+  }
+}
+
+export const updateCompanySettingsFailure = error => {
+  return {
+    type: actionTypes.UPDATE_COMPANY_SETTINGS_FAILURE,
+    payload: error
+  }
+}
+
+export const fetchCompanySettingsStart = () => {
+  return {
+    type: actionTypes.FETCH_COMPANY_SETTINGS_START
+  }
+}
+
+export const fetchCompanySettingsSuccess = settings => {
+  return {
+    type: actionTypes.FETCH_COMPANY_SETTINGS_SUCCESS,
+    payload: settings
+  }
+}
+
+export const fetchCompanySettingsFailure = error => {
+  return {
+    type: actionTypes.FETCH_COMPANY_SETTINGS_FAILURE,
+    payload: error
+  }
+}
+
+export const fetchCompanySettings = (token, id) => {
+  return dispatch => {
+    dispatch(fetchCompanySettingsStart())
+    axios
+      .get(`/settings/${id}/edit?include=providers,company`, {
+        headers: { Authorization: `bearer ${token}` }
+      })
+      .then(res => {
+        const data = normalize(res.data)
+        console.log(data)
+        dispatch(fetchCompanySettingsSuccess(data))
+      })
+      .catch(error => {
+        dispatch(fetchCompanySettingsFailure(error))
+      })
+  }
+}
+
+export const resetEditCompanySettings = () => {
+  return {
+    type: actionTypes.RESET_EDIT_COMPANY_SETTINGS
+  }
+}
